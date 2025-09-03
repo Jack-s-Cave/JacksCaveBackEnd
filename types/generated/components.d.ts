0721439bb261b86f6c4d029313a2accbe3e04bad
Tu Nombre <villafuerte.mariajose046@gmail.com>
2025-09-02 21:55:25 -0600

    feat: partes creadas para asocaiacion + endpoint para llamar asociacion

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,40 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface AsociacionMiembros extends Struct.ComponentSchema {
+  collectionName: 'components_asociacion_miembros';
+  info: {
+    displayName: 'miembros';
+  };
+  attributes: {
+    Cargo: Schema.Attribute.Enumeration<
+      [
+        'Presidente',
+        'Vicepresidente',
+        'Secretario',
+        'Tesorero',
+        'Vocal',
+        'Representante',
+      ]
+    >;
+    curriculum: Schema.Attribute.Text;
+    foto: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+    nombre: Schema.Attribute.String;
+    year_estudiante: Schema.Attribute.Enumeration<
+      [
+        'Primer a\u00F1o',
+        'Segundo a\u00F1o',
+        'Tercer a\u00F1o',
+        'Cuarto a\u00F1o',
+        'Quinto a\u00F1o',
+      ]
+    >;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'asociacion.miembros': AsociacionMiembros;
+    }
+  }
+}
