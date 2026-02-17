808b4c44a42b73cf70463ca77b8033ee6765753f
MEN-GUE <menendezguerrajuan@gmail.com>
2026-02-17 14:49:07 -0600

    refactor(api): estandarizar hooks para podcast y asociación

--- src/api/article-md/controllers/article-md.ts
+++ src/api/article-md/controllers/article-md.ts
@@ -4,4 +4,10 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::article-md.article-md');
+export default factories.createCoreController('api::article-md.article-md', ({ strapi }) => ({
+    // Hook expuesto para personalización futura en el frontend
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+    }
+}));
--- src/api/podcast/controllers/podcast.ts
+++ src/api/podcast/controllers/podcast.ts
@@ -4,4 +4,9 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::podcast.podcast');
+export default factories.createCoreController('api::podcast.podcast', ({ strapi }) => ({
+    async find(ctx) {
+        const { data, meta } = await super.find(ctx);
+        return { data, meta };
+    }
+}));
